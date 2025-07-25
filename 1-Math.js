d0cd814e3071a650c157f5bedabf46a8d366a6be
Rondersem <rondersemcustodio@gmail.com>
2025-07-25 14:16:21 -0300

    movi imagem de ludar

---
+++ 1-Math.js
@@ -0,0 +1,16 @@
+/* 
+
+Math -> Matemática
+    pow(2,2) / potência / 2² = 4
+    sqrt(25,2) / raiz / √25
+    PI / 3,14
+    ceil / teto -> arredondar pra cima
+    floor / chão -> arredondar pra baixo
+
+    random() / número aleatório entre 0 e 1
+
+
+const result = Math.sqrt(25) // raiz quadrada de 25
+console.log(result)
+
+*/
